6faace2943d09b3807a1b9cc28bafb990b1649e3
hamlet <haml262@hotmail.com>
2023-06-23 23:52:04 -0400

    first commit

---
+++ src/galeria/carousel.js
@@ -0,0 +1,48 @@
+const galeria = document.getElementById('galeria');
+const carousel = (direccion) => {
+    const opciones ={
+        root: document.querySelector('.galeria__carousel'),
+        rootMargin: '0px',
+        threshold: 0.9,
+    }
+
+    const observer = new IntersectionObserver((entradas) => {
+            const slidesVisibles = entradas.filter((entrada) => {
+                    if (entrada.isIntersecting === true) {
+                        return entrada;
+                    }
+            });
+
+            if(direccion === 'atras') {
+                const primerSlideVisible = slidesVisibles[0];
+                const indexPrimerSlideVisible = entradas.indexOf(primerSlideVisible);
+
+                if (indexPrimerSlideVisible >= 1) {
+                    entradas[indexPrimerSlideVisible - 1]. target.scrollIntoView({
+                        behavior: 'smooth',
+                        inline: 'start'
+                    });
+                }
+            } else if (direccion === 'adelante') {
+                const ultimaSlideVisible = slidesVisibles[slidesVisibles.length - 1];
+                const indexUltimoSlideVisible = entradas.indexOf(ultimaSlideVisible);
+
+                if (entradas.length - 1 > indexUltimoSlideVisible) {
+                    entradas[indexUltimoSlideVisible + 1]. target.scrollIntoView({
+                        behavior: 'smooth',
+                        inline: 'start'
+                    });
+                }
+            }
+    
+            galeria.querySelectorAll('.galeria__carousel-slide').forEach((slide) => {
+                observer.unobserve(slide)
+             })
+    }, opciones)
+
+    galeria.querySelectorAll('.galeria__carousel-slide').forEach((slide) => {
+        observer.observe(slide)
+    })
+}
+
+export default carousel;
